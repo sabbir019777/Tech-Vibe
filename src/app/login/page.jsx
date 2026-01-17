fd3d547f993b8bfa4c72f7d81e8a0ccf2d96bed4
Sabbir Ahmad <sa9079600@gmail.com>
2026-01-17 15:30:37 +0600

    add login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -41,7 +41,11 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 relative overflow-hidden">
       
-      {/* Background Glow Effects */}
+
+
+
+
+
       <div className="absolute top-[-10%] left-[-10%] w-96 h-96 bg-purple-600 rounded-full mix-blend-multiply filter blur-[128px] opacity-50 animate-blob"></div>
       <div className="absolute bottom-[-10%] right-[-10%] w-96 h-96 bg-blue-600 rounded-full mix-blend-multiply filter blur-[128px] opacity-50 animate-blob animation-delay-2000"></div>
 
